d9139efb88e53693e8496e9fbfbcb62a13cb9dbd
Miguel5040 <miguelssj5040@gmail.com>
2024-03-02 01:37:19 -0700

    responsividad seccion tecnologias arreglado

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,8 +6,7 @@ const Technologies = () => {
         <section className="py-20">
             <div className="container mx-auto max-w-[1200px] w-[90%]">
                 <h1 className="text-3xl pb-14 text-center">Tecnologias</h1>
-                <div className="flex md:flex-col flex-row md:gap-16 justify-center">
-                    <div className="flex md:flex-row flex-col w-full justify-between gap-6">
+                <div className="grid md:grid-cols-7 grid-cols-4 md:gap-16 gap-6">
                         <Language nombre={'React'} archivo={'react'} />
                         <Language nombre={'Python'} archivo={'python'} />
                         <Language nombre={'JavaScript'} archivo={'javascript'} />
@@ -15,8 +14,6 @@ const Technologies = () => {
                         <Language nombre={'CSS'} archivo={'css'} />
                         <Language nombre={'Tailwind CSS'} archivo={'tailwind'} />
                         <Language nombre={'Bootstrap'} archivo={'bootstrap'} />
-                    </div>
-                    <div className="flex md:flex-row flex-col w-full justify-between gap-6">
                         <Language nombre={'VS Code'} archivo={'vscode'} />
                         <Language nombre={'Git'} archivo={'git'} />
                         <Language nombre={'Github'} archivo={'github2'} />
@@ -24,7 +21,6 @@ const Technologies = () => {
                         <Language nombre={'SQL Alchemy'} archivo={'sqlalchemy'} />
                         <Language nombre={'MySQL'} archivo={'mysql'} />
                         <Language nombre={'Jest'} archivo={'jest'} />
-                    </div>
                 </div>
             </div>
         </section>
